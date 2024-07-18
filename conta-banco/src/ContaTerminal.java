f5d9409539e0e533a918a4221518745365a1a6d4
Julio <julioctba12@gmail.com>
2024-07-18 20:25:55 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+     String Nome = "MARIO ANDRADE";
+     int Conta = 1021;
+     String Agencia = "067-8";
+     double Saldo = 237.48;
+
+
+     Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+            String Entrada1 = sc.nextLine();
+        if (Entrada1 == Agencia){
+            System.out.println("Digite sua conta!");
+       };
+
+     sc.close();
     }
 }
